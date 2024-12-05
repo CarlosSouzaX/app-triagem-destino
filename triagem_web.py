103fff0c621843a2bdfe86dd7e0e759f199f9e5b
Carlos Souza <carlos.souza@pitzi.com.br>
2024-12-05 12:22:12 -0300

    Teste C

--- triagem_web.py
+++ triagem_web.py
@@ -22,6 +22,9 @@ st.write("Colunas do DataFrame:", df.columns)
 st.write("Primeiras linhas do DataFrame:")
 st.write(df.head())
 
+# Converter a coluna 'Device' para int64
+df["Device"] = df["Device"].astype(int)
+
 st.write(df.info())
 st.write(df.dtypes)
 
